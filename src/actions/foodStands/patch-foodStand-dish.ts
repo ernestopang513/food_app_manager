256c6c1838d3abbc81b98bf97d99773aab2e70bf
Ernesto Pang <ernestopang513@hotmail.com>
2025-06-01 10:46:36 -0600

    feat(actions): patch dish

---
+++ src/actions/foodStands/patch-foodStand-dish.ts
@@ -0,0 +1,59 @@
+import { foodAppApi } from "../../config/api/foodAppApi";
+
+
+export const patchFoodStandDish = async(id: string = '4c5bef84-8b9c-49fe-a568-92213fde66d5', increment: boolean= true , quantity: number =1, is_active: boolean ) => {
+
+
+    try {
+        const {data} = await foodAppApi.patch(`/food-stand-dish/${id}`, {
+            increment,
+            quantity,
+            is_active
+        }
+        )
+        console.log(data)
+        return data 
+    } catch (error) {
+        console.log(error);
+        throw new Error(`Error actualizando el platillo con id: ${id}`)
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

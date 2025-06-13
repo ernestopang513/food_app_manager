c9203a95056eff1caecbd6b027432428b0e37eda
Ernesto Pang <ernestopang513@hotmail.com>
2025-06-13 10:26:25 -0600

    style(noticeScreen): add some styles

--- src/presentation/components/ui/NoticeScreen.tsx
+++ src/presentation/components/ui/NoticeScreen.tsx
@@ -1,17 +1,19 @@
 import { Button, Layout, Text } from '@ui-kitten/components';
+import { StyleProp, ViewStyle } from 'react-native';
 
 interface Props {
+    style?: StyleProp<ViewStyle>;
     title: string;
     message: string;
     onRetry?: () => void;
 }
 
 
-const NoticeScreen = ({title, message, onRetry}: Props) => {
+const NoticeScreen = ({title, message, onRetry, style}: Props) => {
   return (
-    <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center', padding: 16}} >
+    <Layout style={[{flex: 1, justifyContent: 'center', alignItems: 'center', padding: 16}, style]} >
       <Text  category='h1' status='warning'  >{title}</Text>
-      <Text  category='s1'   >{message}</Text>
+      <Text  category='s1'  style ={{marginTop: 20}} >{message}</Text>
 
         {onRetry && (
             <Button onPress={onRetry}>Retry</Button>

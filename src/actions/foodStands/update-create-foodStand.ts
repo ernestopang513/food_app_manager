a428cb7744cfc009462f69c1e6ae618ab2737665
Ernesto Pang <ernestopang513@hotmail.com>
2025-06-24 01:56:48 -0600

    feat(actions-updateCreatefd): agrega la función de creación de foodStand

--- src/actions/foodStands/update-create-foodStand.ts
+++ src/actions/foodStands/update-create-foodStand.ts
@@ -10,12 +10,11 @@ export const updateCreateFoodStand = (foodStand: Partial<FoodStand>) => {
 
     foodStand.latitude = isNaN(Number(foodStand.latitude)) ? 0 : Number(foodStand.latitude)
     foodStand.longitude = isNaN(Number(foodStand.longitude)) ? 0 : Number(foodStand.longitude)
-    log(foodStand.id)
     if(foodStand.id && foodStand.id !== 'new') {
         log(foodStand.id)
         return updateFoodStand(foodStand);
     }
-    return updateFoodStand(foodStand)
+    return createFoodStand(foodStand)
 }
 
 
@@ -33,5 +32,22 @@ const updateFoodStand = async(foodStand: Partial<FoodStand>) => {
         }
         log(error)
         throw new Error(`Error al actualizar el foodStand`)
+    }
+}
+
+
+const createFoodStand = async(foodStand: Partial<FoodStand>) => {
+    const {id, ...rest} = foodStand;
+
+    try {
+        const {data} = await foodAppApi.post('/food-stands', {
+            ...rest
+        })
+        return data
+    } catch (error) {
+        if(isAxiosError(error)) {
+            log(error.response?.data)
+        }
+        throw new Error('Error al crear el foodStand')
     }
 }

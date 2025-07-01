917c99eaddf0ea0ab52c7f39ef4f156886505305
Ernesto Pang <ernestopang513@hotmail.com>
2025-07-01 01:17:16 -0600

    refactor(waitin/dpScreen): cambia scrollview por flatlist

--- src/presentation/screens/orders/waiting/DeliveryPointScreen.tsx
+++ src/presentation/screens/orders/waiting/DeliveryPointScreen.tsx
@@ -3,7 +3,7 @@ import { StackParamsWaiting } from '../../../routes/orders/waitingStack/WaitingS
 import { Layout, Text, useTheme } from '@ui-kitten/components'
 import { useOrderStore } from '../../../store/orders/useOrdersStore'
 import OrderInfo from '../../../components/orders/OrderInfo'
-import { RefreshControl, ScrollView, View } from 'react-native'
+import { FlatList, RefreshControl, ScrollView, View } from 'react-native'
 import { useEffect, useState } from 'react'
 import AnimatedCardWrapper from '../../../components/ui/animations/AnimatedCardWrapper'
 import FAB from '../../../components/ui/FAB'
@@ -12,110 +12,116 @@ import { useFabStore } from '../../../store/orders/useFabStore'
 import useWaitingOrders from '../../../hooks/orders/queryHooks/useWaitingOrders'
 import useSocketOrders from '../../../hooks/orders/socketHooks/useSocketOrders'
 import useRefreshOnFocus from '../../../hooks/orders/useRefreshOnFocus'
+import ErrorScreen from '../../../components/ui/ErrorScreen'
+import NoticeScreen from '../../../components/ui/NoticeScreen'
+import SkeletonCard from '../../../components/ui/SkeletonCard'
 
-interface Props extends StackScreenProps<StackParamsWaiting, 'DeliveryPointScreen'>{}
+interface Props extends StackScreenProps<StackParamsWaiting, 'DeliveryPointScreen'> { }
 
-const DeliveryPointScreen = ({route}: Props) => {
+const DeliveryPointScreen = ({ route }: Props) => {
 
-  const {deliveryPointId, dpName} = route.params
+  const { deliveryPointId, dpName } = route.params
   const setDeliveryPointId = useOrderStore(state => state.setDeliveryPointId)
-  
+
   const [showFab, setShowFab] = useState(false);
-  
+
   const foodStandId = useOrderStore(state => state.foodStandId);
   const foodStandName = useOrderStore(state => state.foodStandName);
   const userToken = useAuthStore(state => state.token);
   const label = useFabStore(state => state.label);
   const iconName = useFabStore(state => state.iconName);
   const backgroundColor = useFabStore(state => state.backgroundColor);
-  
-  
+
+
   const waitingOrders = useWaitingOrders(foodStandId, deliveryPointId);
-  
-  
-  const {refreshing, handleRefresh} = useRefreshOnFocus(waitingOrders.refetch);
-  
+
+
+  const { refreshing, handleRefresh } = useRefreshOnFocus(waitingOrders.refetch);
+
   useEffect(() => {
     setDeliveryPointId(deliveryPointId);
     console.log('setDeliveryPintId')
 
-  },[])
+  }, [])
 
   const handleOrderStatus = () => {
 
-        setShowFab(true);
+    setShowFab(true);
 
-        setTimeout(() => {
-            setShowFab(false);
-        }, 1500)
-    }
-    
+    setTimeout(() => {
+      setShowFab(false);
+    }, 1500)
+  }
+
+  if (waitingOrders.isError) {
+    return (
+      <ErrorScreen
+        message={waitingOrders.error.message ?? 'Error inesperado'}
+        onRetry={waitingOrders.refetch} />
+    )
+  }
+
 
   return (
-    <Layout style = {{flex: 1, paddingHorizontal: 20, paddingTop: 20}}>
-      <ScrollView
-        refreshControl={
-          <RefreshControl refreshing= {refreshing} onRefresh={handleRefresh}/>
+    <>
+      <FlatList
+        data={waitingOrders.data}
+        initialNumToRender={5}
+        style={{ backgroundColor: '#fff' }}
+        keyExtractor={(item)=> item.id}
+        ListHeaderComponentStyle={{ paddingTop: 15, marginBottom: 10 }}
+        contentContainerStyle={{ paddingHorizontal: 20 }}
+        refreshing = {refreshing}
+        onRefresh={handleRefresh}
+        ListHeaderComponent={
+          <View>
+            <Text category='h2'
+              style={{ textAlign: 'center' }}
+            >{dpName}</Text>
+            <Text category='label'
+              style={{ textAlign: 'center' }}
+            >{foodStandName}</Text>
+          </View>
         }
-      >
-
-      <Text category='h2' 
-        style ={{textAlign: 'center'}}
-        >{dpName}</Text>
-      <Text category='label' 
-        style ={{textAlign: 'center', marginBottom: 20}}
-        >{foodStandName}</Text>
-      
+        renderItem={({ item }) => (
+          <AnimatedCardWrapper>
+            <OrderInfo
+              style={{ marginVertical: 10 }}
+              totalPrice={item.totalPrice}
+              orderId={item.id}
+              userName={item.user.userName}
+              orderDish={item.orderDish}
+              handleOrderStatus={() => handleOrderStatus()}
+            />
+          </AnimatedCardWrapper>
+        )}
+        ListEmptyComponent={
+          waitingOrders.isLoading ?
+            <SkeletonCard /> :
+            <NoticeScreen title='Sin pedidos!' message='Descansa' />
+        }
+      />
       {
-        waitingOrders.data?.length === 0 && !waitingOrders.isError 
-        && !waitingOrders.isLoading && 
-        <View style ={{marginTop: 40}}>
-          <Text style ={{textAlign: 'center'}} status='warning' category='h5' >Por el momento no hay ordenes</Text>
-        </View>
-      }
-      
-      
-      {
-        waitingOrders.data &&
-        waitingOrders.data.map((item) => (
-          <AnimatedCardWrapper
-            key={item.id}
-          >
-
-          <OrderInfo
-          style ={{marginVertical: 10}}
-          totalPrice = {item.totalPrice}
-          orderId = {item.id}
-          userName = {item.user.userName}
-          key={item.id}
-          orderDish = {item.orderDish}
-          handleOrderStatus={() => handleOrderStatus()}
+        showFab && (
+          <FAB
+            iconName={iconName}
+            iconHeight={50}
+            label={label}
+            onPress={() => { }}
+            style={{
+              position: 'absolute',
+              bottom: 30,
+              alignSelf: 'center',
+              zIndex: 100,
+              backgroundColor: backgroundColor,
+              borderColor: 'transparent',
+              borderRadius: 40
+            }}
           />
-         </AnimatedCardWrapper>
-        ))
+
+        )
       }
-
-      <View style ={{height: 50}} />
-
-      </ScrollView>
-      {showFab && (
-  <FAB
-    iconName={iconName}
-    iconHeight={50}
-    label={label}
-    onPress={() => {}}
-    style={{
-      position: 'absolute',
-      bottom: 30,
-      alignSelf: 'center',
-      zIndex: 100,
-      backgroundColor: backgroundColor,
-      borderColor: 'transparent',
-      borderRadius: 40
-    }}
-  />
-)}
-    </Layout>
+    </>
   )
 }
 export default DeliveryPointScreen

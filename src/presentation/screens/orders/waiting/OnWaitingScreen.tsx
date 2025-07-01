1b032b210e51b14d0e810c85a74717efa551e428
Ernesto Pang <ernestopang513@hotmail.com>
2025-07-01 11:37:58 -0600

    refactor(on-waiting-screen): cambia scrollview por flatlist

--- src/presentation/screens/orders/waiting/OnWaitingScreen.tsx
+++ src/presentation/screens/orders/waiting/OnWaitingScreen.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query'
 import { Card, Layout, Text, useTheme } from '@ui-kitten/components'
-import { RefreshControl, ScrollView, StyleSheet, View } from 'react-native'
+import { FlatList, RefreshControl, ScrollView, StyleSheet, View } from 'react-native'
 import { NavigationProp, useFocusEffect, useNavigation } from '@react-navigation/native'
 import { useCallback, useState } from 'react'
 import { getWaitingOrders } from '../../../../actions/orders/get-waiting-orders'
@@ -11,23 +11,24 @@ import NoticeScreen from '../../../components/ui/NoticeScreen'
 import ErrorScreen from '../../../components/ui/ErrorScreen'
 const OnWaitingScreen = () => {
 
-    const foodStandId = useOrderStore(values => values.foodStandId );
+    const foodStandId = useOrderStore(values => values.foodStandId);
     const foodStandName = useOrderStore(value => value.foodStandName);
     const { navigate } = useNavigation<NavigationProp<StackParamsWaiting>>();
     const theme = useTheme();
 
-    
-    
+
+
     const waitingOrders = useQuery({
-      queryKey: ['OrdersWaitingByDeliveryPoints', foodStandId],
-      queryFn: () => {
-        if(!foodStandId) throw new Error('foodStandId es requerido')
-          return getWaitingOrders( foodStandId )},
+        queryKey: ['OrdersWaitingByDeliveryPoints', foodStandId],
+        queryFn: () => {
+            if (!foodStandId) throw new Error('foodStandId es requerido')
+            return getWaitingOrders(foodStandId)
+        },
         staleTime: 0,
         enabled: !!foodStandId,
-        
-      })
-          
+
+    })
+
     const [refreshing, setRefreshing] = useState(false);
 
     useFocusEffect(
@@ -57,104 +58,62 @@ const OnWaitingScreen = () => {
         );
     }
 
-  return (
-    <Layout style={{flex:1, paddingHorizontal: 20, paddingTop: 10}}>
+    if (waitingOrders.isError) {
+        return (
+            <ErrorScreen
+                message={waitingOrders.error.message ?? 'Error inesperado'}
+                onRetry={waitingOrders.refetch} />
+        )
+    }
 
-        <ScrollView
-
-            refreshControl={
-                <RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />
+    return (
+        <FlatList
+            data={waitingOrders.data}
+            initialNumToRender={5}
+            style={{ backgroundColor: '#fff' }}
+            keyExtractor={(item) => item.deliveryPoint.id}
+            ListHeaderComponentStyle={{ paddingTop: 15, marginBottom: 10 }}
+            contentContainerStyle={{ paddingHorizontal: 20 }}
+            refreshing={refreshing}
+            onRefresh={handleRefresh}
+            ListHeaderComponent={
+                <View>
+                    <Text category='h2'
+                        style={{ textAlign: 'center' }}
+                    >{foodStandName}</Text>
+                   
+                </View>
             }
-        >
-            <Text category='h3' style={{textAlign: 'center'}}>{foodStandName}</Text>
-           
-            {
-                waitingOrders.isLoading && <SkeletonCard/>
+            renderItem={({ item }) => (
+                <Card
+                    style={{ marginTop: 20, }}
+                    onPress={() => navigate('DeliveryPointScreen', { deliveryPointId: item.deliveryPoint.id, dpName: item.deliveryPoint.name })}
+                    header={() => <Header nombre={`${item.deliveryPoint.name}`} />}
+                    key={item.deliveryPoint.id}
+                >
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }} >
+
+                        <Text category='h6' >Cantidad de ordenes: </Text>
+                        <Text category='h6'>{item.orders}</Text>
+                    </View>
+                </Card>
+            )}
+            ListEmptyComponent={
+                waitingOrders.isLoading ?
+                    <SkeletonCard /> :
+                    <NoticeScreen title='Nada en el local' message='Aqui estarán las ordenes por local' />
             }
-            {   
-                !waitingOrders.isLoading && !waitingOrders.data && waitingOrders.error &&
-                <ErrorScreen/>
-            }
-
-              {
-                  waitingOrders.data?.length === 0 && !waitingOrders.isError
-                  && !waitingOrders.isLoading &&
-                  <View style={{marginTop: 20, alignItems: 'center', justifyContent: 'center'}}>
-                      <Text category='h5' status='warning'>Por el momento no hay ordenes</Text>
-                  </View>
-              }
-
-            {
-                !waitingOrders.isLoading && !waitingOrders.error && waitingOrders.data && 
-                waitingOrders.data.map(({deliveryPoint, orders}) => {
-                    return (
-                        <Card
-                            style ={{marginTop: 20, }}
-                            onPress={() => navigate('DeliveryPointScreen', {deliveryPointId: deliveryPoint.id, dpName: deliveryPoint.name }) }
-                            header={() => <Header nombre ={`${deliveryPoint.name}`} />}
-                            key={deliveryPoint.id}
-                        >
-                            <View style = {{flexDirection: 'row', justifyContent: 'space-between'}} >
-
-                            <Text category='h6' >Cantidad de ordenes: </Text>
-                            <Text category='h6'>{orders}</Text>
-                            </View>
-                        </Card>
-                    )
-                    
-                })
-            }
-
-        </ScrollView>
-
-    </Layout>
-  )
+        />
+    )
 }
 export default OnWaitingScreen
 
-// interface OrderPreviewProps {
-//     level: "2" | "4";
-//     orderDishes: OrderDishOrderResp[];
-//     user: UserOrderResp;
-// }
 
-
-// const OrderPreview = ({level, orderDishes, user}: OrderPreviewProps) => {
-//     return (
-//         <Layout  level = {level} style = {{padding: 10, borderRadius: 10, marginVertical: 10}} >
-//             <Text category='h6'>{`UserName: ${user.userName}`}</Text>
-
-//             {
-//                 orderDishes.map((item)=>(
-//                     <Layout level={level} style={styles.amount} key={item.id}>
-//                         <Text>{`${item.dish.name}:`}</Text>
-//                         <Text>{item.quantity}</Text>
-//                     </Layout>
-//                 ))
-//             }
-
-//         </Layout>
-//     )
-
-    
-// }
-
-const styles = StyleSheet.create({
-  amount: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    padding: 10,
-    marginVertical: 2,
-    borderRadius: 8,
-  },
-});
-
-
-const Header = ({nombre}: {nombre: string}) => {
+const Header = ({ nombre }: { nombre: string }) => {
     const theme = useTheme();
-  return (
-    <View>
-        <Text category="h4" style = {{ padding: 10, backgroundColor: theme['color-primary-200']}}>{nombre}</Text>
-    </View>
-  )
+    return (
+        <View>
+            <Text category="h4" style={{ padding: 10, backgroundColor: theme['color-primary-200'] }}>{nombre}</Text>
+        </View>
+    )
 }

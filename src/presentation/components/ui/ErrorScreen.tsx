d24d3b39f7c9187e555551ffa5354f13e9179faa
Ernesto Pang <ernestopang513@hotmail.com>
2025-06-30 23:06:44 -0600

    feat(errorScreen): agrega onRetry button

--- src/presentation/components/ui/ErrorScreen.tsx
+++ src/presentation/components/ui/ErrorScreen.tsx
@@ -1,16 +1,23 @@
-import { Icon, Layout, Text, useTheme } from "@ui-kitten/components";
+import { Button, Icon, Layout, Text, useTheme } from "@ui-kitten/components";
 
 interface Props {
   message?: string;
+  onRetry?: () => void;
 }
 
 
-const ErrorScreen = ({message = 'Error inesperado'}: Props) => {
+const ErrorScreen = ({message = 'Error inesperado', onRetry}: Props) => {
   const theme = useTheme()
   return (
-    <Layout style = {{flex: 1, justifyContent: 'center', paddingHorizontal: 20, alignItems: 'center' }} >
-      <Text numberOfLines={5} category='h4' status='danger' >{message}</Text>
+    <Layout style = {{flex: 1, justifyContent: 'center', paddingHorizontal: 20, alignItems: 'center', gap: 20 }} >
+      <Text numberOfLines={5} category='h3' >Error</Text>
+      <Text numberOfLines={5} category='h5' status='danger' >{message}</Text>
       <Icon name='bug-outline' style = {{height: 50, marginTop: 10}} color = {theme['color-danger-500']}  />
+      {
+        onRetry && (
+          <Button size='giant' onPress={onRetry} >Retry</Button>
+        )
+      }
     </Layout>
   )
 }
